acf96eee32e6b2ba4e9e7f4c1974c02ab4a5d87f
Rohit Sen <ts.rohitsen@gmail.com>
2023-10-31 23:56:14 +0600

    v1.0

---
+++ backend/app.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+//Connnectiong to mongoDB
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lrgci3m.mongodb.net/?retryWrites=true&w=majority`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const studentsCollection = client.db("NEUB_Sinfo").collection("students");
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+   
+   
+   
+      //querying all the info to this route
+    // app.get("/students", async (req, res) => {
+    //   const cursor = studentsCollection.find();
+    //   const result = await cursor.toArray();
+    //   res.send(result);
+    // });
+
+
+// querying the DB
+
+    app.get("/students/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await studentsCollection.findOne({ id });
+      res.send(result || "{}");
+    });
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+
+
+run().catch(console.dir);
+
+//................................
+
+app.get("/", (req, res) => {
+  res.send("Helloo working");
+});
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}/`);
+});
--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,19 @@
-function update(){
-    alert("Thanks for your interest! Rohit is working on the backend, it will be available soon :)");
+
+
+function handleClick(event) {
+    event.preventDefault();
+    const uidInput = document.getElementById("uid");
+    const id = uidInput.value;
+    // alert("ID:" + id);
+
+    fetch(`http://localhost:5000/students/${id}`)
+    .then(res=>res.json())
+    .then(data=> {
+        alert( `UserName : ${data.name}\n
+        Blood Group: ${data.bloodGroup}\n
+        Email : ${data.email}\n
+        CGPA: ${data.result}\n
+        Regular Student : ${data.status}` 
+            )})
 }
+
